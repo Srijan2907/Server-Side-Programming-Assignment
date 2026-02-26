c64db95890982c3718a35c0de4f9501cd4c185f6
Srijan2907 <jaiswalsrijan46@gmail.com>
2026-02-26 21:49:30 +0530

    Commit Question 10

---
+++ javaScript assignment/Question10.js
@@ -0,0 +1,8 @@
+function isNull(value) {
+    return value === null;
+}
+
+console.log(isNull(null));      
+console.log(isNull(5));         
+console.log(isNull("hello"));   
+console.log(isNull(undefined));
