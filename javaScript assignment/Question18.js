1eb5df54f4d666b4456d11e5623efcd54bc61cd2
Srijan2907 <jaiswalsrijan46@gmail.com>
2026-03-01 11:33:41 +0530

    Commit Question 18

---
+++ javaScript assignment/Question18.js
@@ -0,0 +1,19 @@
+function swapCase(str) {
+
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+
+        let char = str[i];
+
+        if (char === char.toUpperCase()) {
+            result += char.toLowerCase();
+        } else {
+            result += char.toUpperCase();
+        }
+    }
+
+    return result;
+}
+
+console.log(swapCase("Server Side Programming"));
